727746a979947727925121d1fdb33d103ccbbd2f
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-25 19:48:50 +0000

    Added a widen function to make values wide enough to be written in subsequently larger sized registers

--- src/registers.cpp
+++ src/registers.cpp
@@ -1,86 +1,117 @@
 // generic headers
 #include <iostream>
+#include <type_traits>
+#include <algorithm>
 
 // Project specific headers
 #include <libkdebugger/registers.hpp>
 #include <libkdebugger/bit.hpp>
 #include <libkdebugger/process.hpp>
 
-// reading a specific valued register
-kdebugger::registers::value kdebugger::registers::read(const register_info & info) const {
-	auto bytes = as_bytes(m_Data);
+namespace {
+	// reading a specific valued register
+	kdebugger::registers::value kdebugger::registers::read(const register_info & info) const {
+		auto bytes = as_bytes(m_Data);
 
-	if(info.format == register_format::uint) {
-		switch(info.size) {
-			case 1:
-				return from_bytes<std::uint8_t> (bytes + info.offset);
+		if(info.format == register_format::uint) {
+			switch(info.size) {
+				case 1:
+					return from_bytes<std::uint8_t> (bytes + info.offset);
 
-			case 2:
-				return from_bytes<std::uint16_t> (bytes + info.offset);
+				case 2:
+					return from_bytes<std::uint16_t> (bytes + info.offset);
 
-			case 4:
-				return from_bytes<std::uint32_t> (bytes + info.offset);
+				case 4:
+					return from_bytes<std::uint32_t> (bytes + info.offset);
 
-			case 8:
-				return from_bytes<std::uint64_t> (bytes + info.offset);
+				case 8:
+					return from_bytes<std::uint64_t> (bytes + info.offset);
 
-			default:
-				kdebugger::error::send("Unexpected register size");
+				default:
+					kdebugger::error::send("Unexpected register size");
+			}
 		}
-	}
 
-	else if(info.format == register_format::double_float) {
-		return from_bytes<double> (bytes + info.offset);
-	}
+		else if(info.format == register_format::double_float) {
+			return from_bytes<double> (bytes + info.offset);
+		}
 
-	else if(info.format == register_format::long_double) {
-		return from_bytes<long double> (bytes + info.offset);
-	}
+		else if(info.format == register_format::long_double) {
+			return from_bytes<long double> (bytes + info.offset);
+		}
 
-	else if(info.format == register_format::vector && info.size == 8) {
-		return from_bytes<byte64> (bytes + info.offset);
-	}
-
-	else {
-		return from_bytes<byte128> (bytes + info.offset);
-	}
-}
-
-// writing to a register in bytes
-void sdb::registers::write(const register_info & info, value val) {
-	auto bytes = as_bytes(m_Data);
-
-	// visit -> takes a callback && std::variant to call a function
-	// with the value stored in our type-safe union
-	std::visit([&](auto & v) -> void {
-		if(sizeof(v) == info.size) {
-			auto val_bytes = as_bytes(v);
-
-			std::copy(val_bytes, val_bytes + sizeof(v),
-					bytes + info.offset);
-		}	
+		else if(info.format == register_format::vector && info.size == 8) {
+			return from_bytes<byte64> (bytes + info.offset);
+		}
 
 		else {
-			std::cerr << "kdebugger::register::write called w/ "
-				"mismatched register value and sizes";
-			std::terminate();
+			return from_bytes<byte128> (bytes + info.offset);
 		}
-	}, val);
-
-
-	if(info.type == register_type::fpr) {
-		m_Process->write_fprs(m_Data.i387);
 	}
 
-	else {	
-		// aligns the lowest 3 bits in the address to 0
-		// forced to be 8 byte aligned 
-		auto aligned_offset = info.offset & ~0b111;
-		m_Process->write_user_area(aligned_offset, 
-				from_bytes<std::uint64_t> (bytes + aligned_offset));
+	// writing to a register in bytes
+	void sdb::registers::write(const register_info & info, value val) {
+		auto bytes = as_bytes(m_Data);
+
+		// visit -> takes a callback && std::variant to call a function
+		// with the value stored in our type-safe union
+		std::visit([&](auto & v) -> void {
+			if(sizeof(v) == info.size) {
+				auto val_bytes = as_bytes(v);
+
+				std::copy(val_bytes, val_bytes + sizeof(v),
+						bytes + info.offset);
+			}	
+
+			else {
+				std::cerr << "kdebugger::register::write called w/ "
+					"mismatched register value and sizes";
+				std::terminate();
+			}
+		}, val);
+
+
+		if(info.type == register_type::fpr) {
+			m_Process->write_fprs(m_Data.i387);
+		}
+
+		else {	
+			// aligns the lowest 3 bits in the address to 0
+			// forced to be 8 byte aligned 
+			auto aligned_offset = info.offset & ~0b111;
+			m_Process->write_user_area(aligned_offset, 
+					from_bytes<std::uint64_t> (bytes + aligned_offset));
+		}
+	}
+	
+	// widens a value to store in a larger register
+	template <class T>
+	kdebugger::byte128 widen(const kdebugger::register_info & info, T t) {
+		
+		using namespace kdebugger;
+		if constexpr (std::is_floating_point_v<T>) {
+			if(info.format == register_format::double_float)
+				return to_byte128(static_cast<double> (t));
+
+			if(info.format == register_format::long_double)
+				return to_byte128(static_cast<long double> (t));
+		}
+
+		else if constexpr (std::is_signed_v<T>) {
+			if(info.format == register_format::uint) {
+				switch(info.size) {
+					case 2:
+						return to_byte128(static_cast<std::int16_t> (t));
+
+					case 4:
+						return to_byte128(static_cast<std::int32_t> (t));
+
+					case 8:
+						return to_byte128(static_cast<std::int64_t> (t));
+				}
+			}
+		}
+
+		return to_byte128(t);
 	}
 }
-
-
-
-

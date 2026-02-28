9bde1b846bb1ea0241341e65454a386023b52005
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-28 11:26:06 +0000

    Implemented to_integral function as a wrapper for std::from_chars returning an optional

---
+++ include/libkdebugger/parse.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+// generic includer headers
+#include <charconv>
+#include <stdint>
+#include <optional>
+#include <string_view>
+
+namespace kdebugger {
+
+	template <class T>
+	std::optional<T> to_integral(std::string_view sv, int base = 10) {
+		auto begin = sv.begin();
+
+		if(base == 16 && sv.size() > 1 && begin[0] == '0' && begin[1] == 'x')
+			begin += 2;
+
+		T ret;
+		auto result = std::from_chars(begin, sv.end(), ret, base);
+		if(result.ptr != sv.end())
+			return std::nullopt;
+
+		return ret;
+	}
+}

b8ae02bdb07bf0cfd382365cc07191d1b9b98391
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-28 11:47:36 +0000

    Outlined class for virtual address space - this includes reading the prgoram counter

--- include/libkdebugger/types.hpp
+++ include/libkdebugger/types.hpp
@@ -2,10 +2,25 @@
 
 #include <array>
 #include <cstddef>
+#include <cstdint>
 
 namespace kdebugger {
 
 	using byte64 = std::array<std::byte, 8>;
 	using byte128 = std::array<std::byte, 16>;
+	
+	class virt_addr {
+		
+		private:
+			std::uint64_t m_Addr {0};
 
+		public:
+			virt_addr() = default;
+
+			explicit virt_addr(std::uint64_t addr) : m_Addr {addr} {}
+
+			std::uint64_t addr() const {
+				return m_Addr;
+			}
+	};
 }

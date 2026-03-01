d4448526e3d94a9e6bd3bac340ce95f44b9d1f9a
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-01 09:54:34 +0000

    added tests for iterating over breakpoint sites

--- test/tests.cpp
+++ test/tests.cpp
@@ -259,6 +259,7 @@ TEST_CASE("Cannot find breakpoint site", "[breakpoint]") {
 	REQUIRE_THROW_AS(cproc->breakpoint_sites().get_by_id(44), error);
 }
 
+// in case - tests the breakpoint site vector for size and emptiness
 TEST_CASE("Breakpoint site list size and emptiness", "[breakpoint]") {
 	auto proc = process::launch("targets/run_endlessly");
 	const auto & cproc = proc;
@@ -279,4 +280,27 @@ TEST_CASE("Breakpoint site list size and emptiness", "[breakpoint]") {
 	REQUIRE(proc->breakpoint_sites().size() == 2);
 	REQUIRE(!cproc->breakpoint_sites().empty());
 	REQUIRE(cproc->breakpoint_sites().size() == 2);
+}
+
+// in case - is able to iterate over the breakpoint sites vectoc
+TEST_CASE("Can iterate breakpoint sites", "[breakpoint]") {
+	auto proc = process::launch("targets/run_endlessly");
+	const auto & cproc = proc;
+
+	proc->create_breakpoint_site(virt_addr{42});
+	proc->create_breakpoint_site(virt_addr{43});
+	proc->create_breakpoint_site(virt_addr{44});
+	proc->create_breakpoint_site(virt_addr{45});
+
+	proc->breakpoint_sites().for_each(
+		[addr = 42] (auto & site) mutable {
+			REQUIRE(site.address().addr() == addr++);
+		}
+	);
+
+	cproc->breakpoint_sites().for_each(
+		[addr = 42] (auto & site) mutable {
+			REQUIRE(site.address().addr() == addr++);
+		}
+	);
 }

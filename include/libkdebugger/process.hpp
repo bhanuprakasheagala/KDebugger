55d9204b483ebac740768776bcaf370e37b7e1af
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-02 18:50:59 +0000

    Added read memory template function that returns a given size that is read

--- include/libkdebugger/process.hpp
+++ include/libkdebugger/process.hpp
@@ -89,6 +89,12 @@ namespace kdebugger {
 			std::vector<std::byte> read_memory(virt_addr address, size_t amount) const;
 			void write_memory(virt_addr address, span<const std::byte> data);
 			
+			// read memory as a certain size
+			template <class T> T read_memory_as(virt_addr address) const {
+				auto data = read_memory(address, sizeof(T));
+				return from_bytes<T>(data.data());
+			}
+
 			// resume the current process if halted
 			void resume();
 

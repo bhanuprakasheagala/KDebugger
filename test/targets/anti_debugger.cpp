75bc2e2f7f1bde8814c75806b401454fdcb3aa43
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-06 17:09:32 +0000

    Finished test

--- test/targets/anti_debugger.cpp
+++ test/targets/anti_debugger.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <numeric>
+#include <unistd.h>
 
 void an_innocent_function() {
     std::puts("Putting pineapple on pizza");
@@ -12,4 +13,17 @@ int checksum() {
     auto end   = reinterpret_cast<volatile const char*>(&an_innocent_function_end);
 
     return std::accumulate(start, end, 0);
+}
+
+int main() {
+    auto safe = checksum();
+
+    while(true) {
+        sleep(1);
+
+        if(checksum() == safe)
+            an_innocent_function();
+        else
+            puts("Putting pepperoni on pizza");
+    }
 }

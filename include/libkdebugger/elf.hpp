92268c820289ead1f34b0fce59d228a325831244
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-08 12:05:26 +0000

    Defined header for elf formatted files

---
+++ include/libkdebugger/elf.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+// General include paths
+#include <filesystem>
+#include <elf.h>
+
+namespace kdebugger {
+	// defines the ELF specification we can use to parse executables,
+	// static/dynamic libraries or core dumps
+	class elf {
+		
+		private:
+			int m_Fd;
+			std::filesystem::path m_Path;
+			std::size_t m_FileSize;
+			std::byte * m_Data;
+			Elf64_Ehdr m_Header;
+
+		public:
+			elf(const std::filesystem::path & path);
+		
+			// deletes copy constructor / copy assignment
+			elf(const elf &) = delete;
+			elf & operator = (const elf &) = delete;
+
+			std::filesystem::path path() const {
+				return m_Path;
+			}
+
+			const Elf64_Ehdr & get_header() const {
+				return m_Header;
+			}
+
+			~elf();
+	}
+}
+
+

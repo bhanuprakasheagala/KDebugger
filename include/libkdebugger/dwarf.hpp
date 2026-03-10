864643b79dac05244131ede9ea1928732b0bae76
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-10 17:49:58 +0000

    Added a cursor type for parsing dwarf data and advancing the location of the pointer at a given location in the abbrev table

--- include/libkdebugger/dwarf.hpp
+++ include/libkdebugger/dwarf.hpp
@@ -4,6 +4,7 @@
 
 // I will implement this later... whole lotta constants lmao
 #include <libkdebugger/detail/dwarf.h>
+#include <libkdebugger/types.hpp>
 
 namespace kdebugger {
 	
@@ -20,5 +21,32 @@ namespace kdebugger {
 			const elf * elf_file() const { return m_Elf; }
 	
 			const std::unordered_map<std::uint64_t, abbrev> & get_abbrev_table(std::size_t offset);
+	};
+
+	class cursor {
+		
+		std::span<const std::byte> m_Data;
+		const std::byte* m_Pos;
+
+		public:
+			explicit cursor(kdebugger::span<const std::byte> data) : m_Data {data}, m_Pos {data.begin()} {}
+
+			cursor & operator ++ () {
+				++m_Pos; 
+				return *this; 
+			}
+
+			cursor & operator += (std::size_t size) {
+				m_Pos += size;
+				return *this;
+			}
+
+			const std::byte * positon() const {
+				return m_Pos;
+			}
+
+			bool finished() const {
+				return m_Pos >= m_Data.end();
+			}
 	};
 };

6fcafb92d97cc1f94754e90d05531042f14e4843
TomAndJerrys2 <kylev2005@hotmail.com>
2026-03-11 18:02:25 +0000

    Basic implementation for parsing debug info - very bad and costly code but will improve in the future with caching and better c++20 features

--- src/dwarf.cpp
+++ src/dwarf.cpp
@@ -7,6 +7,7 @@ const std::unordered_map<std::uint64_t, kdebugger::abbrev> & kdebugger::dwarf::g
 	return m_AbbrevTables.at(offset);
 }
 
+// will refactor this later
 std::unordered_map<std::uint64_t, kdebugger::abbrev> parse_abbrev_table(const kdebugger::elf & obj, std::size_t size) {
 	cusor cur(obj.get_section_contents(".debug_abbrev"));
 	cur += offset;
@@ -16,6 +17,24 @@ std::unordered_map<std::uint64_t, kdebugger::abbrev> parse_abbrev_table(const kd
 
 	do {
 		// parse entries
+		code = cur.uleb128();
+		auto tag = cur.uleb128();
+		auto has_children = static_cast<bool>(cur.u8());
+
+		std::vector<kdebugger::attr_spec> attr_specs;
+		std::uint64_t attr = 0;
+		do {
+			attr = cur.uleb128();
+			auto form = cur.uleb128();
+			
+			if(attr != 0)
+				attr_specs.push_back(kdebugger::attr_spec {attr, form});
+
+		} while(attr != 0);
+
+		if(code != 0)
+			table.emplace(code, kdebugger::abbrev {code, tag, has_children, std::move(attr_specs)});
+		
 	} while(code != 0);
 
 	return table;
